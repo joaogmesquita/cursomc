f1f0106b60a71bbb7f33bda559f17ca0c518f55e
João Guilherme <joao.g.mesquita.m@gmail.com>
2022-08-29 15:44:33 -0300

    Deletando com delete

--- src/main/java/com/example/joao/resources/CategoriaResources.java
+++ src/main/java/com/example/joao/resources/CategoriaResources.java
@@ -33,4 +33,18 @@ public class CategoriaResources {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/com/example/joao/services/CategoriaService.java
+++ src/main/java/com/example/joao/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.example.joao.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.example.joao.domain.Categoria;
@@ -18,13 +19,34 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 
-		return obj.orElseThrow(()-> new ObjectNotFoundExcepiton("Objeto não encontrado! Id: "+ id + ", Tipo: "+ Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundExcepiton(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.example.joao.services.exception.DataIntegrityViolationException("Não é possivel apagar categoria com produtos" );
 		
+			
+		}
+		
+
 	}
 
 }
